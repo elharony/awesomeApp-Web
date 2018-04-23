9800d1481caedfe5eb0033e376233baabbf71bd0
elharony <p.yahyaelharony@gmail.com>
2018-04-23 21:48:06 +0200

    refactor: Move the functionality code 'app.js'

--- js/app.js
+++ js/app.js
@@ -16,6 +16,78 @@ var config = {
     messagingSenderId: "523185544926"
 };
 firebase.initializeApp(config);
+
+
+
+
+/*
+ *
+ * Login
+ * 
+ */
+ // Initialize the FirebaseUI Widget using Firebase.
+ var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ var uiConfig = {
+   callbacks: {
+     signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+       // User successfully signed in.
+       // Return type determines whether we continue the redirect automatically
+       // or whether we leave that to developer to handle.
+       return true;
+     },
+     uiShown: function() {
+       // The widget is rendered.
+       // Hide the loader.
+       document.getElementById('loader').style.display = 'none';
+     }
+   },
+   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+   signInFlow: 'popup',
+   signInSuccessUrl: './',
+   signInOptions: [
+     // Leave the lines as is for the providers you want to offer your users.
+     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+   ],
+   // Terms of service url.
+   tosUrl: '<your-tos-url>'
+ };
+
+ // The start method will wait until the DOM is loaded.
+ ui.start('#firebaseui-auth-container', uiConfig);
+
+
+ const loggedInDiv = document.querySelector(".signedIn");
+ const loggedOutDiv = document.querySelector(".signedOut");
+
+ firebase.auth().onAuthStateChanged(function(user) {
+   if (user) {
+     loggedInDiv.style.display = "block";
+     loggedOutDiv.style.display = "none";
+     document.querySelector("#done").innerHTML = user;
+   } else {
+     loggedInDiv.style.display = "none";
+     loggedOutDiv.style.display = "block";
+   }
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var db = firebase.firestore();
 
 const docRef = db.doc("helpData/tracks");
