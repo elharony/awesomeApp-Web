4315a3aefc77369808f953fe34db96307efb374d
klaraborowska <33237744+klaraborowska@users.noreply.github.com>
2018-05-12 10:56:45 +0200

    Style filter by language test

--- js/app.js
+++ js/app.js
@@ -388,7 +388,7 @@ function getStudents(containerElement, projectName, queryFlag) {
                 const closeFilter = document.createElement("span");
                 closeFilter.classList.add("closeFilter");
                 closeFilter.innerHTML = "close"
-                studentsCount.appendChild(closeFilter);
+                infoBox.appendChild(closeFilter);
 
                 //close filter event
                 closeFilter.addEventListener('click', ()=>{
